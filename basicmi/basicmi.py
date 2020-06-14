f5779cc34a06584f8f2c4986c59db3728d1fd7ac
dans-acc <danv123456789@gmail.com>
2020-06-14 08:59:05 +0100

    Started working on code

---
+++ basicmi/basicmi.py
@@ -0,0 +1,14 @@
+# Module imports.
+import numpy as np
+import scipy as sp
+import matplotlib as plt
+
+
+# The main function.
+def main():
+    pass
+
+
+# When module is run as a singleton.
+if __name__ == '__main__':
+    main()
