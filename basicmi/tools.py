43c9646a5342f520295b0034a5f9a7fd1e35c4bd
dans-acc <danv123456789@gmail.com>
2020-06-29 14:34:38 +0100

    Added tools

--- basicmi/tools.py
+++ basicmi/tools.py
@@ -1,74 +1,53 @@
 import pathlib
+
+import mne
 import scipy.io as sio
 
-
-def get_proj_path():
-
-    # Get the project path relative to this file.
-    path = pathlib.Path(__file__)
-    if path is None:
-        return
-    return pathlib.Path(path.parent.parent)
+# Commonly used paths relative to the tools.py file.
+PROJ_DIR_PATH = pathlib.Path(pathlib.Path(__file__).parent.parent)
+DATA_DIR_PATH = pathlib.Path(PROJ_DIR_PATH.joinpath('data'))
+MI_DATA_DIR_PATH = pathlib.Path(DATA_DIR_PATH.joinpath('mi'))
 
 
-def get_proj_sub_path(sub_path='basicmi'):
+def get_subj_epochs(subj_id, preload=True, equalise_events_ids=None):
 
-    # Get a path relative to the projects root directory.
-    proj_path = get_proj_path()
-    if proj_path is None:
-        return
-    return pathlib.Path(proj_path.joinpath(sub_path))
-
-
-def get_matching_paths(from_path, pattern):
-
-    # Check that the parameters are valid.
-    if from_path is None or pattern is None:
-        return None
-    elif not from_path.is_dir():
+    # Sanity checks.
+    if subj_id is None:
         return None
 
-    # Match all sub paths.
-    paths = [pathlib.Path(path) for path in from_path.glob(pattern)]
-    return paths
-
-
-def get_mat_items(mat_path, mat_keys=None):
-
-    # Check that the mat file exists.
-    if mat_path is None or not mat_path.exists():
+    # Get the subject path.
+    subj_path = pathlib.Path(MI_DATA_DIR_PATH.joinpath('S%d//epochs_epo.fif'))
+    if not subj_path.exists() or not subj_path.is_file() or subj_path.is_dir():
         return None
 
-    # Read the mat file, if no keys, return the entire dict.
-    mat_file = sio.loadmat(mat_path)
-    if mat_keys is None:
-        return mat_file
-    else:
+    # Get the subjects epochs.
+    epochs = mne.read_epochs(str(subj_path.absolute()), preload=preload)
+    if epochs is None:
+        return None
 
-        # Only get a subset of the mat keys.
-        return {key: mat_file.get(key) for key in mat_keys}
+    # Whether the classes should be equalised.
+    if equalise_events_ids is not None:
+        epochs.equalize_event_counts(event_ids=equalise_events_ids)
 
-
-def get_dir_paths(dir_path, regex='*', only_files=False, bad_files=None):
-
-    # Check that the dir path is valid.
-    if dir_path is None or not dir_path.is_dir():
-        raise NotADirectoryError('dir_path is None or not a directory.')
-
-    # Read all paths within the directory.
-    paths = dir_path.glob(regex)
-    if only_files:
-        paths = [path for path in paths if path.is_file() and not path.is_dir()]
-
-    # If no bad files are defined, return all paths.
-    if bad_files is None or not bad_files.any():
-        return paths
-    else:
-
-        # Filter out all of the bad files from the data set.
-        return [path for path in paths if path.name not in bad_files]
+    return epochs
 
 
-# Common paths for the project.
-ROOT_DIR_PATH = get_proj_path()
-DATA_DIR_PATH = get_proj_sub_path('data')
+def get_proj_epochs(subj_ids, preload=True, equalise_events_ids=None):
+
+    # Sanity checks.
+    if subj_ids is None:
+        return None
+    elif not subj_ids:
+        return {}
+
+    # Get epochs for each of the subjects.
+    subj_epochs = {}
+    for subj_id in subj_ids:
+        subj_epoch = get_subj_epochs(subj_id=subj_id, preload=preload,
+                                     equalise_events_ids=equalise_events_ids)
+        if subj_epoch is None:
+            continue
+        subj_epochs[subj_id] = subj_epoch
+
+    return subj_epochs
+
