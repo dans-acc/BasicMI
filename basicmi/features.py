e0ddc3d2c8029c526e977e07249c2e27f2160202
Doombana <danv123456789@gmail.com>
2020-07-29 20:09:37 +0100

    Added the ability to generate window feature matrix

--- basicmi/features.py
+++ basicmi/features.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import List, Dict, Callable
+from typing import List, Dict, Callable, Tuple
 
 import mne
 import numpy as np
@@ -40,19 +40,6 @@ def get_subject_psd_feats(subject_epochs, t_min, t_max, freq_bands, n_jobs=3, ap
         _logger.debug('Returning samples_x_feats matrix. No classes are appended to the end.')
         return np.array(samples_x_feats_mtx) if as_np_arr else samples_x_feats_mtx
 
-    # Get the labels associated with each of the epochs.
-    epochs_labels = utils.get_epochs_labels(epochs=subject_epochs)
-    if epochs_labels is None:
-        raise ValueError('Unable to get the associated labels for epoch: %s', str(subject_epochs))
-
-    # Epochs and labels must be the same length.
-    assert len(epochs_labels) == len(samples_x_feats_mtx)
-
-    # Include the classes into the feature matrix.
-    for i in range(len(samples_x_feats_mtx)):
-        features = samples_x_feats_mtx[i]
-        features.append(epochs_labels[i])
-
     # Return the matrix containing the samples x features (with classes appended to the end of the features.)
     return np.array(samples_x_feats_mtx) if as_np_arr else samples_x_feats_mtx
 
@@ -67,7 +54,45 @@ def get_psds_feats(epochs, t_min, t_max, freq_bands, n_jobs=3, append_classes=Fa
     return subject_features
 
 
-def get_features(epochs: Dict[int, mne.Epochs], target: Callable = None, kwargs=None) -> Dict[int, np.ndarray]:
+def get_psd_features(subject_epochs: mne.Epochs, windows: List[Tuple[float, float]], bands: List[Tuple[float, float]],
+                     n_jobs: int = 10) -> np.ndarray:
+
+    # Feature matrix represents windows * samples (epochs) * features (theta, alpha, and beta bands)
+    mtx = []
+    for window_idx in range(len(windows)):
+        samples = []
+        for epoch_idx in range(len(subject_epochs)):
+            samples.append([])
+        mtx.append(samples)
+
+    # Loop through each window and generate features.
+    for window_idx in range(len(windows)):
+
+        # For each of the windows generate PSDS features for the defined bands.
+        t_min, t_max = windows[window_idx]
+
+        _logger.info('Generating PSD features for window: %s - %s.', str(t_min), str(t_max))
+
+        for f_min, f_max in bands:
+
+            # Returns a matrix in the shape of n_epochs, n_channels, n_freqs.
+            _logger.debug('Generating PSD features for band: %s - %s', str(f_min), str(f_max))
+            psds, freqs = mne.time_frequency.psd_multitaper(inst=subject_epochs, tmin=t_min, tmax=t_max,
+                                                            fmin=f_min, fmax=f_max, proj=True, n_jobs=n_jobs)
+
+            # Loop through each epoch index, then each channel, adding the mean FFT PSD to the feature matrix.
+            for epoch_idx in range(len(psds)):
+                for channel in psds[epoch_idx]:
+                    mean_channel_psd = channel.mean()
+                    mtx[window_idx][epoch_idx].append(mean_channel_psd)
+
+    # Finally, convert the list matrix into a numpy matrix and return.
+    np_mtx = np.asarray(mtx)
+    _logger.debug('Generated feature matrix: %s', str(np_mtx.shape))
+    return np_mtx
+
+
+def get_features(epochs: Dict[int, mne.Epochs], target: Callable = None, **kwargs) -> Dict[int, np.ndarray]:
     features = {}
     for subject_id, subject_epochs in epochs.items():
         subject_features = target(**kwargs)
