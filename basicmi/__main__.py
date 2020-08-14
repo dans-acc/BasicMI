c146a94c812423dc5017bca97308b6467c840495
Doombana <danv123456789@gmail.com>
2020-08-14 16:21:13 +0100

    Working on supporting a new dataset

--- basicmi/__main__.py
+++ basicmi/__main__.py
@@ -5,6 +5,8 @@ import logging
 import os
 import pathlib
 
+from typing import List, Dict, Tuple
+
 import mne
 import tensorflow as tf
 import numpy as np
@@ -15,42 +17,44 @@ from basicmi import montages, subjects, utils, features, train
 _logger = utils.create_logger(name=__name__, level=logging.DEBUG)
 
 
-def load_or_generate_fif_feats():
-    pass
+def load_or_generate_fif_feats(dataset: str = '2020', load_subjects: List[int] = None, drop_labels: List[int] = None,
+                               equalise_event_ids: List[str] = None, new_trial_labels: Dict[int, int] = None,
+                               bands: List[Tuple[int, int]] = None, windows: List[Tuple[float, float]] = None,
+                               n_image_grid_points: int = 32, normalise_image: bool = True, edgeless_image: bool = True,
+                               electrode_locations: np.ndarray = None) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
 
+    # If the dataset is none, assume the latest dataset is being used.
+    if dataset is None:
+        dataset = '2020'
 
-def load_or_generate_mat_feats():
-    pass
+    _logger.info('Loading or generating features and images using the .FIF and MNE (using the %s dataset)', dataset)
 
-
-def main():
-
-    # Init library pseudo random number generators.
-    pseudo_random_seed = 2018
-    np.random.seed(pseudo_random_seed)
-    tf.set_random_seed(pseudo_random_seed)
-
-    # Read and load the electrode cap locations.
-    electrode_locations = montages.get_neuroscan_montage(apply_azim=True)
-
-    # Attributes defining what data should be loaded.
-    load_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    drop_labels = [3]
-    equalise_event_ids = None
+    # If the subjects that are to be loaded does not match, assume all are being loaded.
+    if load_subjects is None:
+        load_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
     # Read and load epochs that we are concerned with.
-    epochs = subjects.get_epochs(subject_ids=load_subjects, preload=True, equalise_event_ids=equalise_event_ids,
-                                 add_subject_id_info=True, drop_labels=drop_labels)
+    epochs = subjects.get_epochs(dataset=dataset, subject_ids=load_subjects, preload=True,
+                                 equalise_event_ids=equalise_event_ids, add_subject_id_info=True,
+                                 drop_labels=drop_labels)
 
     # Generate leave one out cross validation fold pairs based on the loaded epochs.
     trial_ids, trial_labels, fold_pairs = subjects.get_loocv_fold_pairs(epochs=epochs)
 
-    # Since class labels must start at 1; remap the values such that the meet the latter requirement.
-    subjects.remap_trail_labels(trial_labels, new_labels={1: 0, 2: 1, 3: 2})
+    # If none, the default assumes all classes are present.
+    if new_trial_labels is None:
+        new_trial_labels = {1: 0, 2: 1, 3: 2}
 
-    # The bands and windows defining the features that are to be extracted.
-    bands = [(4, 8), (8, 12), (12, 30)]
-    windows = utils.generate_windows(start=-2, stop=5, step=0.2)
+    # Since class labels must start at 1; remap the values such that the meet the latter requirement.
+    subjects.remap_trail_labels(trial_labels, new_labels=new_trial_labels)
+
+    # The bands define the frequencies from which the features are to be extracted; if none, assumes defined.
+    if bands is None:
+        bands = [(4, 8), (8, 12), (12, 30)]
+
+    # If there are no windows present, generate default ones.
+    if windows is None:
+        windows = utils.generate_windows(start=-2, stop=5, step=1)
 
     try:
 
@@ -58,6 +62,7 @@ def main():
         epoch_feats_path = utils.get_dict_path(
             from_path=pathlib.Path(pathlib.Path(__file__).parent.joinpath('features//psd')),
             dictionary={
+                'Dataset': dataset,
                 'Subjects': load_subjects,
                 'Equalised': equalise_event_ids,
                 'Dropped': drop_labels,
@@ -94,17 +99,13 @@ def main():
 
     _logger.debug('Features shape: %s', str(feats.shape))
 
-    # Attributes defining image properties.
-    n_image_grid_points = 32
-    normalise_image = True
-    edgeless_image = False
-
     try:
 
-        # Generate a new path to where images are stored.
+        # Generate a 2014 path to where images are stored.
         feats_images_path = utils.get_dict_path(
             from_path=pathlib.Path(pathlib.Path(__file__).parent.joinpath('images//psd')),
             dictionary={
+                'Dataset': dataset,
                 'Subjects': load_subjects,
                 'Equalised': equalise_event_ids,
                 'Dropped': drop_labels,
@@ -133,6 +134,10 @@ def main():
 
     else:
 
+        # If the electrode locations have not already been loaded, load them.
+        if electrode_locations is None:
+            electrode_locations = montages.get_neuroscan_montage(apply_azim=True)
+
         # Generate images based on the features.
         _logger.info('Generating images for PSD features...')
         images = utils.generate_images(features=feats, electrode_locations=electrode_locations,
@@ -142,6 +147,50 @@ def main():
         # Save the images if a valid filepath exists; optimises successive runs as they do not need to be regenerated.
         if feats_images_path is not None:
             utils.save_mat_items(mat_path=feats_images_path, mat_items={'images': images})
+
+    return images, trial_labels, fold_pairs
+
+
+def load_or_generate_mat_feats():
+    pass
+
+
+def main():
+
+    # Init library pseudo random number generators.
+    pseudo_random_seed = 2018
+    np.random.seed(pseudo_random_seed)
+    tf.set_random_seed(pseudo_random_seed)
+
+    # Read and load the electrode cap locations.
+    electrode_locations = montages.get_neuroscan_montage(apply_azim=True)
+
+    # Attributes defining what data should be loaded.
+    dataset = '2020'
+    load_subjects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    drop_labels = [3]
+    equalise_event_ids = None
+    new_trial_labels = {1: 0, 2: 1, 3: 2}
+
+    # The bands and windows defining the features that are to be extracted.
+    bands = [(4, 8), (8, 12), (12, 30)]
+    windows = utils.generate_windows(start=-2, stop=5, step=1)
+
+    # Attributes defining image properties.
+    n_image_grid_points = 32
+    normalise_image = True
+    edgeless_image = False
+
+    # Generate the images, labels and folds associated with prior parameters.
+    images, trial_labels, fold_pairs = load_or_generate_fif_feats(dataset=dataset, load_subjects=load_subjects,
+                                                                  drop_labels=drop_labels,
+                                                                  equalise_event_ids=equalise_event_ids,
+                                                                  new_trial_labels=new_trial_labels, bands=bands,
+                                                                  windows=windows,
+                                                                  n_image_grid_points=n_image_grid_points,
+                                                                  normalise_image=normalise_image,
+                                                                  edgeless_image=edgeless_image,
+                                                                  electrode_locations=electrode_locations)
 
     # Model parameters.
     model_type = 'lstm'
